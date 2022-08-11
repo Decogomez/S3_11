0e7613bfe003a470dc0e78d2ea2f57c1283af6a1
decogomez <melquiadesagomes@gmail.com>
2022-08-11 20:15:44 -0300

    S3-11

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let nome = prompt("entre com o nome")
+
+let sobrenome = prompt ("entre com o sobrenome")
+
+let cidadeatual = prompt ("digite a cidade onde mora")
+
+let anonascimento = +prompt ("entre com o ano de nascimento")
+
+let anoatual = 2022
+
+let idadeatual = anoatual-anonascimento
+
+console.log ("eu sou o(a)", nome, sobrenome, ",tenho", idadeatual, "anos e moro em", cidadeatual)
+
+alert (` "Eu sou o ${nome} ${sobrenome} tenho ${idadeatual} anos e moro em ${cidadeatual}" `)
